83e6484e1f921e1b45f7ef0787b88fcda6c8fe61
mobai <mobai@wacai.com>
2022-04-12 19:01:27 +0800

    add logging module

--- celery-basic-01/main.py
+++ celery-basic-01/main.py
@@ -3,8 +3,12 @@
 # @FileName: main.py
 # @Software: PyCharm
 from tasks import demo01, demo02
+from utils.logger import Logger
 
+lg = Logger()
 
 if __name__ == "__main__":
+    lg.logger.info("test")
+    lg.logger.warning("test")
     res01 = demo01.delay(1, 2)
     res02 = demo02.delay(4, 4)
--- celery-basic-01/tasks.py
+++ celery-basic-01/tasks.py
@@ -7,18 +7,31 @@ import time
 from celery import Celery
 from config import CELERY_BROKER, CELERY_BACKEND
 
+# celery初始化基本配置
 app = Celery('celery-basic-01', broker=CELERY_BROKER, backend=CELERY_BACKEND)
 
 
 @app.task(name='task_demo_01')
 def demo01(x, y):
+    """
+    name显式指定task名称
+    :param x:
+    :param y:
+    :return:
+    """
     time.sleep(10)
     return x + y
 
 
 @app.task(bind=True, name='task_demo_02')
 def demo02(self, x, y):
-    print(type(self))
+    """
+    设定bind=True，可通过self将task实例传入
+    :param self:
+    :param x:
+    :param y:
+    :return:
+    """
     time.sleep(10)
     return x * y
 
